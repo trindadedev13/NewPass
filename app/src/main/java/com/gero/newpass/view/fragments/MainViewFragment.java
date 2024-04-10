803c9efd0b81bbc4e36eff923413593631045833
gero <gerotto.alessandro08@gmail.com>
2024-04-11 00:42:21 +0200

    added vibration

--- app/src/main/java/com/gero/newpass/view/fragments/MainViewFragment.java
+++ app/src/main/java/com/gero/newpass/view/fragments/MainViewFragment.java
@@ -11,7 +11,6 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
