36796e87b3257ba4c53076734be040169bcd7949
gero <gerotto.alessandro08@gmail.com>
2024-04-12 00:05:07 +0200

    it doesnt work and i dont know why

--- app/src/main/java/com/gero/newpass/repository/ResourceRepository.java
+++ app/src/main/java/com/gero/newpass/repository/ResourceRepository.java
@@ -1,17 +1,53 @@
 package com.gero.newpass.repository;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.StringRes;
+
+import com.gero.newpass.SharedPreferences.SharedPreferencesHelper;
 
 public class ResourceRepository {
     private final Context context;
 
     public ResourceRepository(Context context) {
-        this.context = context.getApplicationContext(); // Usa il contesto dell'applicazione per evitare memory leaks
+        this.context = context.getApplicationContext();
     }
 
     public String getString(@StringRes int resId) {
-        return context.getString(resId);
+        String currentLanguage = getCurrentLanguage();
+
+        Log.i("235234", currentLanguage);
+
+        String resourceFileName = "strings";
+
+        if ("en".equals(currentLanguage)) {
+            resourceFileName = "strings-en";
+        }
+        if ("it".equals(currentLanguage)) {
+            resourceFileName = "strings-it";
+        }
+        if ("es".equals(currentLanguage)) {
+            resourceFileName = "strings-es";
+        }
+        if ("fr".equals(currentLanguage)) {
+            resourceFileName = "strings-fr";
+        }
+
+        Log.i("235234", resourceFileName);
+
+        String resourceName = context.getResources().getResourceEntryName(resId); // login_done
+
+        Log.i("235234", resourceName);
+
+        int resourceId = context.getResources().getIdentifier(resourceName, "string", context.getPackageName());
+
+        Log.i("235234", context.getResources().getString(resourceId));
+
+        return context.getResources().getString(resourceId);
+    }
+
+    public String getCurrentLanguage() {
+        return SharedPreferencesHelper.getCurrentLanguage(context);
     }
 }
