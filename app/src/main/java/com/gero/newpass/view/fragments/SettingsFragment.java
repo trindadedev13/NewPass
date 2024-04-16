dc443a14b92e44350cefe6c042fbc575539d2f41
gero <gerotto.alessandro08@gmail.com>
2024-04-16 12:25:45 +0200

    ready to add import/export

--- app/src/main/java/com/gero/newpass/view/fragments/SettingsFragment.java
+++ app/src/main/java/com/gero/newpass/view/fragments/SettingsFragment.java
@@ -1,5 +1,7 @@
 package com.gero.newpass.view.fragments;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
@@ -11,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,16 +22,17 @@ import android.widget.ImageView;
 
 import com.gero.newpass.R;
 import com.gero.newpass.SharedPreferences.SharedPreferencesHelper;
+import com.gero.newpass.database.DatabaseServiceLocator;
 import com.gero.newpass.databinding.FragmentSettingsBinding;
 import com.gero.newpass.utilities.VibrationHelper;
 import com.gero.newpass.view.activities.MainViewActivity;
 
 public class SettingsFragment extends Fragment {
     private ImageButton buttonBack;
-    private ImageView IVGithub, IVShare, IVContact, IVLanguage;
+    private ImageView IVGithub, IVShare, IVContact, IVLanguage, IVExport, IVImport;
     private FragmentSettingsBinding binding;
-
     private Boolean isDarkModeSet;
+    private static final int PICK_FILE_REQUEST_CODE = 1;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -101,6 +105,41 @@ public class SettingsFragment extends Fragment {
             languageDialogFragment.setCancelable(false);
             languageDialogFragment.show(requireActivity().getSupportFragmentManager(), "Language Dialog");
         });
+
+        IVExport.setOnClickListener(v -> {
+
+            VibrationHelper.vibrate(requireContext(), getResources().getInteger(R.integer.vibration_duration1));
+
+            //TODO: export database -> open file manager exporting db as "password.db" and let the user saves it
+        });
+
+        IVImport.setOnClickListener(v -> {
+
+            VibrationHelper.vibrate(requireContext(), getResources().getInteger(R.integer.vibration_duration1));
+
+            //TODO: import database -> open file manager and let the user pick a .db
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("*/*");
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+            startActivityForResult(Intent.createChooser(intent, "Select File"), PICK_FILE_REQUEST_CODE);
+
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_FILE_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri selectedFileUri = data.getData();
+                Log.i("23895", "file caricato: " + selectedFileUri);
+                Log.i("23895", "db: " + DatabaseServiceLocator.getDatabaseHelper());
+                //DatabaseServiceLocator.setDatabaseHelper(selectedFileUri);
+
+            }
+        }
     }
 
     private void initViews(FragmentSettingsBinding binding) {
@@ -109,6 +148,8 @@ public class SettingsFragment extends Fragment {
         IVShare = binding.imageViewShare;
         IVContact = binding.imageViewContact;
         IVLanguage = binding.imageViewLanguage;
+        IVExport = binding.imageViewExport;
+        IVImport = binding.imageViewImport;
 
         isDarkModeSet = SharedPreferencesHelper.isDarkModeSet(this.requireActivity().getApplicationContext());
 
