eb397f149e073b2a9143d645cbd09ae2baf7ffa3
gero <gerotto.alessandro08@gmail.com>
2024-05-13 00:10:12 +0200

    Fixed status bar in light theme

--- app/src/main/java/com/gero/newpass/utilities/SystemBarColorHelper.java
+++ app/src/main/java/com/gero/newpass/utilities/SystemBarColorHelper.java
@@ -8,18 +8,22 @@ import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.gero.newpass.SharedPreferences.SharedPreferencesHelper;
+
 public class SystemBarColorHelper {
 
     public static void changeBarsColor(Context context, int color) {
-        try {
-            Window window = getWindow(context);
-            View decor = window.getDecorView();
-            decor.setSystemUiVisibility(0);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(ContextCompat.getColor(context, color));
-            window.setNavigationBarColor(ContextCompat.getColor(context, color));
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("The provided color is invalid.");
+        if (SharedPreferencesHelper.isDarkModeSet(context)) {
+            try {
+                Window window = getWindow(context);
+                View decor = window.getDecorView();
+                decor.setSystemUiVisibility(0);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(ContextCompat.getColor(context, color));
+                window.setNavigationBarColor(ContextCompat.getColor(context, color));
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("The provided color is invalid.");
+            }
         }
     }
 
