8ed6017e1ef92de0145dc22a5132e415f9273270
gero <gerotto.alessandro08@gmail.com>
2024-05-13 00:37:52 +0200

    pre release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.gero.newpass"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.1.9"
+        versionCode = 12
+        versionName = "1.1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
