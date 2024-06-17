d5df2f78aeebaa91f5a61de320e60e86587b825d
gero <gerotto.alessandro08@gmail.com>
2024-06-17 22:41:18 +0200

    pre release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.gero.newpass"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.1.10"
+        versionCode = 13
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -61,11 +61,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity:1.9.0")
-    implementation("androidx.databinding:databinding-runtime:8.4.0")
+    implementation("androidx.databinding:databinding-runtime:8.5.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
