8ddc4346d17b53bce5d23bbe324fce92c96bdb3c
aquilesTrindade <sempedra075@gmail.com>
2024-08-11 03:51:54 -0300

    feat: use MaterialAlertDialog instead AlertDialog

--- app/src/main/java/com/gero/newpass/utilities/DialogHelper.java
+++ app/src/main/java/com/gero/newpass/utilities/DialogHelper.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 
 import androidx.security.crypto.EncryptedSharedPreferences;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 import com.gero.newpass.R;
 import com.gero.newpass.database.DatabaseHelper;
 import com.gero.newpass.encryption.HashUtils;
@@ -23,16 +25,16 @@ import java.security.spec.InvalidKeySpecException;
 public class DialogHelper {
 
     public static void showChangePasswordDialog(Context context, EncryptedSharedPreferences encryptedSharedPreferences) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        var dialog = new MaterialAlertDialogBuilder(context);
         LayoutInflater inflater = LayoutInflater.from(context);
         View dialogView = inflater.inflate(R.layout.dialog_change_password, null);
-        builder.setView(dialogView);
+        dialog.setView(dialogView);
 
         EditText firstInput = dialogView.findViewById(R.id.first_input);
         EditText secondInput = dialogView.findViewById(R.id.second_input);
         EditText thirdInput = dialogView.findViewById(R.id.third_input);
 
-        builder.setTitle(R.string.settings_change_password)
+        dialog.setTitle(R.string.settings_change_password)
                 .setPositiveButton(R.string.update_alertdialog_yes, (dialog, id) -> {
 
                     String inputOldPassword = firstInput.getText().toString();
@@ -63,19 +65,17 @@ public class DialogHelper {
                     }
                 })
                 .setNegativeButton(R.string.update_alertdialog_no, (dialog, id) -> dialog.cancel());
-
-        AlertDialog dialog = builder.create();
         dialog.show();
     }
 
     public static void showExportingDialog(Context context) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        var dialog = new MaterialAlertDialogBuilder(context);
         LayoutInflater inflater = LayoutInflater.from(context);
         View dialogView = inflater.inflate(R.layout.dialog_export_or_import_db, null);
-        builder.setView(dialogView);
+        dialog.setView(dialogView);
         EditText input = dialogView.findViewById(R.id.input);
 
-        builder.setTitle(R.string.export_database)
+        dialog.setTitle(R.string.export_database)
                 .setPositiveButton(R.string.confirm, (dialog, id) -> {
                     String password = input.getText().toString();
 
@@ -87,19 +87,17 @@ public class DialogHelper {
 
                 })
                 .setNegativeButton(R.string.cancel, (dialog, id) -> dialog.cancel());
-
-        AlertDialog dialog = builder.create();
         dialog.show();
     }
 
     public static void showImportingDialog(Context context, Uri fileURL) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        var dialog = new MaterialAlertDialogBuilder(context);
         LayoutInflater inflater = LayoutInflater.from(context);
         View dialogView = inflater.inflate(R.layout.dialog_export_or_import_db, null);
-        builder.setView(dialogView);
+        dialog.setView(dialogView);
         EditText input = dialogView.findViewById(R.id.input);
 
-        builder.setTitle(R.string.import_database)
+        dialog.setTitle(R.string.import_database)
                 .setPositiveButton(R.string.confirm, (dialog, id) -> {
                     String password = input.getText().toString();
                     try {
@@ -109,8 +107,6 @@ public class DialogHelper {
                     }
                 })
                 .setNegativeButton(R.string.cancel, (dialog, id) -> dialog.cancel());
-
-        AlertDialog dialog = builder.create();
         dialog.show();
     }
 }
