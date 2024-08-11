0b1653ec545544fc14a506985a53c92636716f34
aquilesTrindade <sempedra075@gmail.com>
2024-08-11 02:52:35 -0300

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,14 +61,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0-alpha03")
+    implementation("com.google.android.material:material:1.13.0-alpha05"")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity:1.9.0")
-    implementation("androidx.databinding:databinding-runtime:8.5.0")
+    implementation("androidx.activity:activity:1.9.1")
+    implementation("androidx.databinding:databinding-runtime:8.5.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     implementation("net.zetetic:android-database-sqlcipher:4.4.0")
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
