1ce7b88fc3bdc2b9f44a830c727f68351c1ae416
Aquiles Trindade <devsuay@gmail.com>
2024-09-19 04:03:18 -0300

    chore(deps): update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,11 +61,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.7.0-alpha03")
-    implementation("com.google.android.material:material:1.13.0-alpha05")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.13.0-alpha06")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity:1.9.1")
-    implementation("androidx.databinding:databinding-runtime:8.5.1")
+    implementation("androidx.activity:activity:1.9.2")
+    implementation("androidx.databinding:databinding-runtime:8.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
